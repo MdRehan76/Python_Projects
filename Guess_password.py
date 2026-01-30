6a561db9da6d97c64d60b536a3eaa9cbd42afb9a
MdRehan76 <mohd.rehanmalim@gmail.com>
2026-01-31 00:22:11 +0530

    Guess Password file

---
+++ Guess_password.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+char_valves = string.ascii_letters + string.punctuation + string.digits
+pass_len = int(input("Enter the length of password = "))
+
+password = ""
+
+for i in range(0,pass_len):
+    password += random.choice(char_valves)
+
+print(f"Random Password : {password}")
+
