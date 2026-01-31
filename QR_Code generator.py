d42a8426114ca0f6a65f2d2bd13f99d31f0063bc
MdRehan76 <mohd.rehanmalim@gmail.com>
2026-01-31 14:15:14 +0530

    QRcode_Generator

---
+++ QR_Code generator.py
@@ -0,0 +1,12 @@
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version = 1,
+                   error_correction= qrcode.constants.ERROR_CORRECT_H,
+                   box_size = 20, border = 4)
+
+qr.add_data("https://github.com/MdRehan76")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color = "pink",back_color = "Black")
+img.save("Rehan_GitHub_Profile.png")
